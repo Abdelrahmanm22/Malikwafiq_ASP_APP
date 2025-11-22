8f39d8ff3ec3ecddb856fd82067ad46869c60e7b
Abdelrahmanm22 <abdra1396@gmail.com>
2025-11-22 20:50:11 +0200

    add time lectures view

---
+++ Areas/Front/Controllers/LectureController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Malek_wafik.Areas.Front.Controllers
+{
+    [Area("Front")]
+    public class LectureController : Controller
+    {
+    
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
